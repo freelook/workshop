74c931aa3d5dc58c38b6d6f308850be29c182269
freelook <freelook@mail.ua>
2016-06-20 09:52:05 -0700

    - add template

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -13,9 +13,9 @@ var wrench = require('wrench');
  *  This will load all js or coffee files in the gulp directory
  *  in order to load all gulp tasks
  */
-wrench.readdirSyncRecursive('./gulp').filter(function(file) {
+wrench.readdirSyncRecursive('./gulp').filter(function (file) {
   return (/\.(js|coffee)$/i).test(file);
-}).map(function(file) {
+}).map(function (file) {
   require('./gulp/' + file);
 });
 
@@ -26,4 +26,8 @@ wrench.readdirSyncRecursive('./gulp').filter(function(file) {
  */
 gulp.task('default', ['clean'], function () {
   gulp.start('build');
+});
+
+gulp.task('tdd', function () {
+  gulp.start('test:auto');
 });
