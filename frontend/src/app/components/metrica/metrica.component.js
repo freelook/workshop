aec0e3b8aa1867a86122b4e6925ff52b05e59da9
freelook <freelook@mail.ua>
2016-06-29 10:52:32 -0700

    add google analytics

--- frontend/src/app/components/metrica/metrica.component.js
+++ frontend/src/app/components/metrica/metrica.component.js
@@ -3,10 +3,27 @@
 
   angular
     .module('frontend')
-    .factory('metrica', function ($window, $rootScope) {
+    .factory('metrica', function ($window, $rootScope, $location) {
       return {
         init: function () {
+          (function (i, s, o, g, r, a, m) {
+            i.GoogleAnalyticsObject = r;
+            i[r] = i[r] || function () {
+                (i[r].q = i[r].q || []).push(arguments);
+              };
+            i[r].l = 1 * new Date();
+            a = s.createElement(o);
+            m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m);
+          })($window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+          $window.ga('create', 'UA-80064844-1', 'auto');
           var route = $rootScope.$on('$routeChangeSuccess', function () {
+            if ($window.ga) {
+              $window.ga('send', 'pageview', {page: decodeURIComponent($location.url())});
+            }
             $window.$.get('https://mc.yandex.ru/watch/38127630');
           });
           return route;
