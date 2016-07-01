a99d8a304121fcfb28ce45f606b8be3bbaaebfe4
freelook <freelook@mail.ua>
2016-06-30 20:16:07 -0700

    add quiz -- cssnano

--- frontend/gulp/build.js
+++ frontend/gulp/build.js
@@ -51,7 +51,7 @@ gulp.task('html', ['inject', 'partials'], function () {
     .pipe(cssFilter)
     // .pipe($.sourcemaps.init())
     .pipe($.replace('../../bower_components/material-design-iconfont/iconfont/', '../fonts/'))
-    .pipe($.cssnano())
+    .pipe($.cssnano({zindex: false}))
     .pipe($.rev())
     // .pipe($.sourcemaps.write('maps'))
     .pipe(cssFilter.restore)
